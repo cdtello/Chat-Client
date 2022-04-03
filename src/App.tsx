5edcf5c82874cd0fe33dedd252533b7abeb4fe58
cdtello <carlos.tello@correounivalle.edu.co>
2022-04-03 14:57:00 -0500

    testClient

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useCallback, useRef } from 'react'
 import { ChatClient } from './chat-client';
 
-const URL = 'ENTER_YOUR_WEBSOCKET_URL_HERE';
+const URL = 'wss://8dkwo40oa7.execute-api.us-east-1.amazonaws.com/production';
 
 const App = () => {
 
