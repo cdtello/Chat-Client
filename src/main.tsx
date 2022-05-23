19e7f89aba01c6be4238576f861bfea9cbdb4578
cdtello <carlos.tello@correounivalle.edu.co>
2022-05-22 23:03:30 -0500

    test2

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,44 @@ import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <>
+    <div>
+      <p>
+        <h1>¿Felicidad...?</h1>
+      <br></br>
+      <br></br>
+        Todos hemos pasado por situaciones complejas en nuestra vida.<br></br>
+        Todos hemos tenido que sortear dificultades y demostrarnos a nosotros mismos lo que somos capaces de hacer.<br></br>
+        Todos hemos tenido que elegir lo que consideramos que necesitamos por encima de lo que queremos.<br></br>
+        Todos hemos reflexionado en algun momento y nos hemos detenido a pensar si estamos en el camino correcto.<br></br>
+        Todos hemos logrado cosas inimaginables, que nunca pensamos alcanzar, y nos sentimos orgullosos de nosotros mismos.<br></br><br></br>
+        Pero...<br></br>
+        ¿Todos nos hemos cuestionado si en realidad somos felices...?<br></br>
+        ¿Nuestra felicidad debemos sacrificarla por quedar bien con alguien, o simplemente por brindarle estabilidad emocional a otra persona por agradecimiento?<br></br>
+        ¿Debemos cambiar nuestra forma de ser, nuestro comportamiento, nuestros gustos, nuestras costumbres, nuestros circulos sociales.... Nuestra esencia "lo que nos ha definido toda nuestra existencia", solo por aparentar ser felices, o por darle gusto a otros?<br></br>
+        ¿Debemos estar en un lugar en el que nos sentimos en una carcel... cuando antes eramos felices sintiendonos libres, autenticos, con ganas de comernos el mundo y que los limites eran solo nuestra mente...?<br></br>
+
+        como estas, podemos hacernos cualquier cantidad de preguntas que tal vez tengan respuestas con "Depende de...", "Pero es que es tan....", "pero es que era....", o respuestas muy complejas.<br></br>
+        pero al final del dia, todos, todas, y todes, valen una mierda....<br></br>
+        Vida solo hay una, la Vida no se sacrifica por alguien o por algo que muy en nuestro interior sabemos que no vale la pena.<br></br>
+        lastimosamente, cuando la llama de nuestra vida se apague, no tenemos botón para reiniciar, para replantear... ya el juego habría iniciado y habría finalizado.<br></br>
+
+        en este juego llamado vida, no hay Ganadores, no hay Perdedores. lo único que nos queda, es aprovecharla, solo hay una "1". <br></br>
+        no tenemos mas oportunidades... podemos ganar mucho dinero, podemos ganar poco, podemos ganar algo intermedio... y estar en ese sube y baja. <br></br>
+        Peroooo, lo unico que se nos consume de forma lineal y constante, es el tiempo. El tiempo no negocia, el tiempo solamente corre en una dirección<br></br>
+        de manera indiscriminada. 
+
+        siempre preguntemonos 3 cosas... Soy Feliz?, sigo conservando mi esencia?, si me encontrara a mi mismo en un mundo paralelo, estaria orgulloso de lo que hago con mi tiempo?...
+      </p>
+      <p>
+        
+      </p>
+      <p>
+        
+      </p>
+    </div>
+    </>
   </React.StrictMode>,
   document.getElementById('root')
 )
